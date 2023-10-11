514454dab493b7ed96ff72ec0cabbf4201626afa
MATEUS PADILHA CARESTIATO DANIEL <202308065361@alunos.ibmec.edu.br>
2023-10-11 16:53:54 -0300

    Configurando Biblioteca

---
+++ src/pages/Primeiro.jsx
@@ -0,0 +1,11 @@
+import LinkSegundo from "../components/LinkSegundo";
+
+const Primeiro = ()=> {
+ return (<div>
+    <h1>Mini titulo</h1>
+    <p>Meu texto</p>
+    <LinkSegundo/>
+    </div>);
+};
+
+export default Primeiro;
